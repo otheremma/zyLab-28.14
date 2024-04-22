00fcb8aa3497af06ba3bfe84da607f01e0b7a47b
Rojas49098588 <167337265+Rojas49098588@users.noreply.github.com>
2024-04-22 11:35:05 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include "ItemToPurchase.h"
+using namespace std;
+
+ShoppingCart(string name, string date){
+  
+}
+string GetCustomerName(){
+  return customerName;
+}
+
+ string GetDate(){
+   return currentDate;
+ }     
+
+void AddItem(item){
+  for (int i = 0; i < cartItems.size(); i++){
+    cartItems.at(i) = item;
+  }
+}
+
+void RemoveItem(name){
+  //remove item
+}
+
+void ModifyItem(item){
+  //modify item
+}
+      
+int GetNumItemsInCart(){
+  return cartItems.size();
+}
+
+double GetCostofCart(){
+  //return cost of cart
+}
+      
+void PrintTotal(){
+  for (int i = 0; i < cartItems.size(); i++){
+    cout << cartItems.at(i);
+  }
+}      
+
+void PrintDescriptions(){
+  //cout descriptions
+}
+      
+    
+};
 
